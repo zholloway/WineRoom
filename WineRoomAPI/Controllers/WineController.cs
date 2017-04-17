415c9262547c6b54fd3b1926cd742fb77f045451
Zack <zholloway92@gmail.com>
2017-04-16 20:53:16 -0400

    get individual/add/delete wine added

--- WineRoomAPI/Controllers/WineController.cs
+++ WineRoomAPI/Controllers/WineController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 using WineRoomAPI.Services;
 
 namespace WineRoomAPI.Controllers
@@ -15,14 +16,40 @@ namespace WineRoomAPI.Controllers
         WineServices wineServices = new WineServices(PathToWineRoomDatabase);
 
         //get all wine
-        [HttpGet]
+        [System.Web.Http.HttpGet]
         public IHttpActionResult GetAllWine()
         {
             return Ok(wineServices.GetAllWine());
         }
+
         //get individual wine
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetIndividualWine(int id)
+        {
+            return Ok(wineServices.GetIndividualWineByID(id));
+        }
+
         //create wine
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult AddWine(FormCollection collection)
+        {
+            wineServices.AddWine(collection);
+            return Ok("New wine successfully added to the collection.");
+        }
+
         //update wine
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult EditWine(int id)
+        {
+            return Ok("The wine was successfully updated.");
+        }
+
         //delete wine
+        [System.Web.Http.HttpDelete]
+        public IHttpActionResult DeleteWine(int id)
+        {
+            wineServices.DeleteWine(id);
+            return Ok("The wine was successfully deleted from the collection.");
+        }
     }
 }
--- WineRoomAPI/Services/WineServices.cs
+++ WineRoomAPI/Services/WineServices.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using WineRoomAPI.Models;
 
 namespace WineRoomAPI.Services
@@ -32,6 +33,73 @@ namespace WineRoomAPI.Services
             Connection.Close();
 
             return wineList;
+        }
+
+        public Wine GetIndividualWineByID(int id)
+        {
+            var wine = new Wine();
+
+            var query = "Select * FROM Wine WHERE ID=@ID";
+            var cmd = new SqlCommand(query, Connection);
+            cmd.Parameters.AddWithValue("@ID", id);
+
+            Connection.Open();
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                wine = new Wine(reader);
+            }   
+
+            return wine;
+        }
+
+        public void DeleteWine(int id)
+        {
+            var query = "DELETE FROM Wine WHERE ID=@ID";
+            var cmd = new SqlCommand(query, Connection);
+            cmd.Parameters.AddWithValue("@ID", id);
+
+            Connection.Open();
+            var reader = cmd.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+        public void AddWine(FormCollection collection)
+        {
+            var query = "INSERT INTO Wine (OwnerID, NumberOfBottles, Format, " +
+                "Year, GrapeType, Vineyard, DateAdded, Location, DrinkableStart, " +
+                "Drinkable End, PurchasePrice, MarketPrice, Favorite, Region, Color, " +
+                "Tags) " +
+                "Values (@OwnerID, @NumberOfBottles, @Format, @Year, @GrapeType, " +
+                "@Vineyard, @DateAdded, @Location, @DrinkableStart, @DrinkableEnd, " +
+                "@PurchasePrice, @MarketPrice, @Favorite, @Region, @Color, @Tags)";
+
+            var cmd = new SqlCommand(query, Connection);
+            cmd.Parameters.AddWithValue("@OwnerID", collection["OwnerID"]);
+            cmd.Parameters.AddWithValue("@NumberOfBottles", collection["NumberOfBottles"]);
+            cmd.Parameters.AddWithValue("@Format", collection["Format"]);
+            cmd.Parameters.AddWithValue("@Year", collection["Year"]);
+            cmd.Parameters.AddWithValue("@GrapeType", collection["GrapeType"]);
+            cmd.Parameters.AddWithValue("@Vineyard", collection["Vineyard"]);
+            cmd.Parameters.AddWithValue("@DateAdded",DateTime.Now);
+            cmd.Parameters.AddWithValue("@Location", collection["Location"]);
+            cmd.Parameters.AddWithValue("@DrinkableStart", collection["DrinkableStart"]);
+            cmd.Parameters.AddWithValue("@DrinkableEnd", collection["DrinkableEnd"]);
+            cmd.Parameters.AddWithValue("@PurchasePrice", collection["PurchasePrice"]);
+            cmd.Parameters.AddWithValue("@MarketPrice", collection["MarketPrice"]);
+            cmd.Parameters.AddWithValue("@Favorite", collection["Favorite"]);
+            cmd.Parameters.AddWithValue("@Region", collection["Region"]);
+            cmd.Parameters.AddWithValue("@Color", collection["Color"]);
+            cmd.Parameters.AddWithValue("@Tags", collection["Tags"]);
+
+            Connection.Open();
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+        public void EditWine(int id)
+        {
+            var query = "UPDATE Book SET []";
         }
     }
 }
